820adae535ecd8378b6b31955deecc12cc0f8b7a
Francelli Nakamura <franci.nakamura100@gmail.com>
2022-04-26 19:11:59 -0700

    Changed timer to 15 seconds

--- src/Game.js
+++ src/Game.js
@@ -9,7 +9,7 @@ const useGameState = () => {
   const [stars, setStars] = useState(utils.random(1, 9));
   const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
   const [candidateNums, setCandidateNums] = useState([]);
-  const [secondsLeft, setSecondsLeft] = useState(20);
+  const [secondsLeft, setSecondsLeft] = useState(15);
 
   useEffect(() => {
     if (secondsLeft > 0 && availableNums.length > 0) {
