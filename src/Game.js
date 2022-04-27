6930d96cbde352dc7029f16720f113c2fe9c4399
Francelli Nakamura <franci.nakamura100@gmail.com>
2022-04-26 19:18:39 -0700

    Changed timer to 15 seconds

--- src/Game.js
+++ src/Game.js
@@ -70,7 +70,8 @@ const Game = (props) => {
   return (
     <div className="game">
       <div className="help">
-        Pick 1 or more numbers that sum to the number of stars
+        Pick 1 or more numbers that sum to the number of stars before the timer
+        runs out.
       </div>
       <div className="body">
         <div className="left">
